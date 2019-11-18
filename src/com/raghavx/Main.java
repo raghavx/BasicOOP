9ef53dbe6c453d46fed1949f2f69a8ec7557793d
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-18 19:27:59 +0530

    Data type

--- src/com/raghavx/Main.java
+++ src/com/raghavx/Main.java
@@ -24,20 +24,27 @@ public class Main {
 		 */
 		// integer data type 
 		byte b ; // -128 to 127 
+		System.out.println(b);
 		short s; // -32,768 to 32,767
+		System.out.println(s);
 		int i; // -2^31 to 2^31 - 1
+		System.out.println(i);
 		long l;  // -2^63 to 2^63 - 1
-		
+		System.out.println(l);
 		// decimal / floating point 
 		float f; // single precision 32 bit 
+		System.out.println(f);
 		double d;// double  precision 64 bit 
+		System.out.println(d);
 		
 		// 
 		boolean bt; // 1 bit 
-		
+		System.out.println(bt);
 		//
-		char c; // 16 bit unicode 
+		char c, cc; // 16 bit unicode 
 		
+		System.out.println(cc);
+		System.out.println();
 		System.out.println(Short.MIN_VALUE);
 		System.out.println(Integer.MIN_VALUE);
 		System.out.println(Long.MIN_VALUE);
