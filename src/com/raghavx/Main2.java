ba83b54fb14ddbf58b9677cc60e61992007c7747
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-18 19:33:48 +0530

    Data type

--- src/com/raghavx/Main2.java
+++ src/com/raghavx/Main2.java
@@ -3,8 +3,14 @@ package com.raghavx;
 public class Main2 {
 
 	public static void main(String[] args) {
-
-		System.out.println("My Execution 2");
+										// 0,1,2
+		int[] arr_with_values = new int[] {1,2,3};
+		
+		float[] ar_without_values = new float[10];
+		for(int i = 0; i < 10; i++) {
+			System.out.println(ar_without_values[i]);
+		}
+		
 	}
 
 }
