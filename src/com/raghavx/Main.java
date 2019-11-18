6536724cde419f3a74913284b6fb0e8d1d673594
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-18 19:21:57 +0530

    Data type

--- src/com/raghavx/Main.java
+++ src/com/raghavx/Main.java
@@ -17,7 +17,49 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("My execution started ");
+
+		/*
+		 * Primitive 
+		 * 
+		 */
+		// integer data type 
+		byte b ; // -128 to 127 
+		short s; // -32,768 to 32,767
+		int i; // -2^31 to 2^31 - 1
+		long l;  // -2^63 to 2^63 - 1
+		
+		// decimal / floating point 
+		float f; // single precision 32 bit 
+		double d;// double  precision 64 bit 
+		
+		// 
+		boolean bt; // 1 bit 
+		
+		//
+		char c; // 16 bit unicode 
+		
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		
+		// print the max values 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
