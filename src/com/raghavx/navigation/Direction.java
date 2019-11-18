aead86b3fd96051482fb3f50bdf6fc5f9f8dc263
Raghvendra Sahu <rx.sahu@gmail.com>
2019-11-18 19:01:05 +0530

    Enums discussed

--- src/com/raghavx/navigation/Direction.java
+++ src/com/raghavx/navigation/Direction.java
@@ -1,8 +1,14 @@
 package com.raghavx.navigation;
 
 /**
- * Add more directions 
- * @author raghav
+ * Add more directions
+ * 
+ * @author raghav An enum type is a special data type that enables for a
+ *         variable to be a set of predefined constants. The variable must be
+ *         equal to one of the values that have been predefined for it. Common
+ *         examples include compass directions (values of NORTH, SOUTH, EAST,
+ *         and WEST) and the days of the week.
+ *
  *
  */
 public enum Direction {
